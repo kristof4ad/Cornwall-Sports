e05c2026e2d97dd3673729a38d3d0ac0c6cd6576
kristof4ad <ikristof@Yosemite-Mac.local>
2015-03-28 15:47:54 +0000

    Cornwall Sports

---
+++ Cornwall Sports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cornwall Sports
+//
+//  Created by ikristof on 27/03/2015.
+//  Copyright (c) 2015 ikristof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
